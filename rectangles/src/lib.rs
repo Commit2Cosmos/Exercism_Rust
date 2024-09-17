4394b01e929981c668ab4343be36b38f4a5739be
Anton Belov <anton.belov.al7@gmail.com>
2024-09-17 09:44:48 +0100

    rectangles solved

---
+++ rectangles/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn count(lines: &[&str]) -> u32 {
+    let height = lines.len();
+    let width = lines[0].len();
+
+    for i in 0..lines.len() {
+        for j in 0..lines[0].len() {
+
+        }
+    }
+}
