e3513513b8d2af16fffe33b7c20835e547cea856
Anton Belov <anton.belov.al7@gmail.com>
2025-03-15 11:52:01 +0700

    collatz-conjecture solved

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,10 +1,20 @@
-// use std::iter::successors;
-
+use std::iter::successors;
 
 pub fn collatz(n: u64) -> Option<u64> {
-    let mut counter = 0;
-
-    fn recur(n: u64) {
-        
+    if n == 0 {
+        return None;
     }
+
+    Some(
+        successors(Some(n), |&x| {
+        if x == 1 {
+            None
+        } else if x % 2 == 0 {
+            Some(x / 2)
+        } else {
+            Some(x * 3 + 1)
+        }
+        })
+        .count() as u64 - 1
+    )
 }
